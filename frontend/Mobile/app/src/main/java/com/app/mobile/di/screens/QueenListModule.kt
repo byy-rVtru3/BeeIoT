8769ba1866338c152254572eddd12a8ae92a4e7d
Avtobot22 <fedotkinna@softalt.ru>
2025-12-23 21:17:42 +0700

    fix(di): register GetHivePreviewUseCase in QueenListModule

--- frontend/Mobile/app/src/main/java/com/app/mobile/di/screens/QueenListModule.kt
+++ frontend/Mobile/app/src/main/java/com/app/mobile/di/screens/QueenListModule.kt
@@ -1,5 +1,6 @@
 package com.app.mobile.di.screens
 
+import com.app.mobile.domain.usecase.hives.hive.GetHivePreviewUseCase
 import com.app.mobile.domain.usecase.hives.queen.GetQueensUseCase
 import com.app.mobile.presentation.ui.screens.queen.list.viewmodel.QueenListViewModel
 import org.koin.core.module.dsl.factoryOf
@@ -9,6 +10,7 @@ import org.koin.dsl.module
 val queenListModule = module {
 
     factoryOf(::GetQueensUseCase)
+    factoryOf(::GetHivePreviewUseCase)
 
     viewModelOf(::QueenListViewModel)
 }
